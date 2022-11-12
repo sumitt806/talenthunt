0a6cc480c8c6829d057e4e6aca19d2aa1461b47a
Sumit <you@example.com>
2022-11-12 21:23:44 +0530

    Test

--- resources/views/web/layouts/app.blade.php
+++ resources/views/web/layouts/app.blade.php
@@ -46,6 +46,8 @@
     <script src="{{ asset('web/js/html5shiv.min.js') }}"></script>
     <script src="{{ asset('web/js/respond.min.js') }}"></script>
     <![endif]-->
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 </head>
 <body>
 <!-- Header Start -->
